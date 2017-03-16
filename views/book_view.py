1f37d2577c1744df5458ff6a5049643fded2b68b
alenX <genhaoai@gmail.com>
2017-03-17 07:13:43 +0800

    新增索引页面

--- views/book_view.py
+++ views/book_view.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
 
+# 图书趋势分析
+from flask import Flask, app
+
+app = Flask(__name__)
+
+douban_url = 'https://book.douban.com/latest?icn=index-latestbook-all'  # 新书速递
+
+
+@app.route('/collect_douban_book')
+def collect_douban_book():
+    pass
