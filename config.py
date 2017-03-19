616abb972f7f037f53dd0fe309db4706b1173a8f
wangss <genhaoai@gmail.com>
2017-03-19 11:11:58 +0800

    sqlite调整

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+import os
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 # DEBUG = True
 # SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:1128@localhost:3306/douban?charset=utf8'
-SQLALCHEMY_DATABASE_URI = 'sqlite:////douban.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'douban.sqlite')
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
