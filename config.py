b966d408babb9a2ab5bd9f62da93a846e3fd1643
alenX <genhaoai@gmail.com>
2017-03-19 10:50:09 +0800

    db调整

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 from flask import Flask
 app = Flask(__name__)
 # DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:1128@localhost:3306/douban?charset=utf8'
-# SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/user.db'
+# SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:1128@localhost:3306/douban?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////douban.db'
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
